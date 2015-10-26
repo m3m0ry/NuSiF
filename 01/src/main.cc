90134ff534f02cdcf3d01d8e7c5344845f06d321
Jan Hoenig <hrominium@gmail.com>
2015-10-26 09:24:51 +0100

    done 01

--- 01/src/main.cc
+++ 01/src/main.cc
@@ -6,31 +6,34 @@
 
 int main( int argc, char** argv )
 {
-	auto filereader = new FileReader();
-	filereader->readFile("arraytest.txt");
+	if(argc < 2)
+	{
+		std::cerr << "No configuration file given" << std::endl;
+		std::cerr << "Usage: " << argv[0] << "<config_file>" << std::endl;
+		return -1;
+	}
 
+	internal::progress("Starting to process file:", 0, 10);
+	auto filereader = new FileReader();
+	filereader->readFile(argv[1]);
 
 	filereader->printParameters();
 
-	internal::progress("test", 0, 10);
-	internal::progress("", 50, 100);
-	internal::progress("test2", 200,200);
+	internal::progress("File read", 200,200);
 
+	int height = filereader->getIntParameter("height");
+	int width = filereader->getIntParameter("width");
+	Real initial = filereader->getRealParameter("initial");
+
+
+	int x = filereader->getIntParameter("x");
+	int y = filereader->getIntParameter("y");
 
 	std::cout << "2D array" << std::endl;
-	Array * bla = new Array(20, 20);
-	bla->fill(42);
+	Array * bla = new Array(height, width);
+	bla->fill(initial);
+	(*bla)(x,y) = (*bla)(x,y) * 2;
 	bla->print();
-	delete bla;
-
-
-	std::cout << "1D array" << std::endl;
-	bla = new Array(20);
-	bla->fill(42);
-	bla->print();
-	Real test = (*bla)(19); //should fail
-	ASSERT_MSG(true, "test");
-	std::cout << test << std::endl;
 	delete bla;
 
 	delete filereader;
