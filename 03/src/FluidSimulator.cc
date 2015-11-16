841c23adf565c568e7c1b6d4330bb58a4987b954
Jan Hoenig <hrominium@gmail.com>
2015-11-16 16:51:58 +0100

    last fixes

--- 03/src/FluidSimulator.cc
+++ 03/src/FluidSimulator.cc
@@ -52,16 +52,16 @@ void FluidSimulator::computeFG()
 		G(i, jmax_) = v(i, jmax_);
 	}
 
-	for(size_t i = 1; i < imax_; ++i)
+	for(size_t j = 1; j <= jmax_; ++j)
 	{
-		for(size_t j = 1; j <= jmax_; ++j)
+		for(size_t i = 1; i < imax_; ++i)
 		{
 			F(i,j) = u(i,j) + dt_* ( (1.0 / re_) * ( grid_.d2udx2(i,j) + grid_.d2udy2(i,j)) - grid_.du2dx(i,j,gamma_) - grid_.duvdy(i,j, gamma_) + gx_);
 		}
 	}
-	for(size_t i = 1; i <= imax_; ++i)
+	for(size_t j = 1; j < jmax_; ++j)
 	{
-		for(size_t j = 1; j < jmax_; ++j)
+		for(size_t i = 1; i <= imax_; ++i)
 		{
 			G(i,j) = v(i,j) + dt_* ( (1.0 / re_) * ( grid_.d2vdx2(i,j) + grid_.d2vdy2(i,j)) - grid_.duvdx(i,j, gamma_) - grid_.dv2dy(i,j, gamma_) + gy_);
 		}
